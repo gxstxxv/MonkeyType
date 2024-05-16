163b79a96b1f1a4c6f7661e0f2f46fa5efd8863e
gxstxxv <gustav.gehricke@gmx.de>
2024-05-16 13:03:07 +0200

    added manual language selection

--- src/RandomSentence.java
+++ src/RandomSentence.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 
 public class RandomSentence {
 
-    private final static String word_list = "words.txt";
+		private final static int language_index = 1;
+		private final static String[][] languages = {
+				{"german","201"},
+				{"english","999"},
+		};
 
     public static ArrayList<ArrayList<Character>> create_sentence(){
 
-        int sentence_length = 7 + (int) (Math.random() * (15 - 7 + 1));
+        int sentence_length = 7 + (int) (Math.random() * (10 - 7 + 1));
         ArrayList<ArrayList<Character>> sentence = new ArrayList<>();
 
         for(int i = 0; i < sentence_length; i++){
@@ -36,8 +40,8 @@ public class RandomSentence {
         while (word.length() <= 1) {
 
             try {
-                word = Files.readAllLines(Paths.get(word_list))
-                        .get((int) (Math.random() * (999)));
+                word = Files.readAllLines(Paths.get(languages[language_index][0] + ".txt"))
+                        .get((int) (Math.random() * (Integer.parseInt(languages[language_index][1]))));
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
